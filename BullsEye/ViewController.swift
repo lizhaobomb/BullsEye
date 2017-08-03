bdb1734bb3325add018f91baf87f543cf145ae94
lizhaobomb <lizhaobomb@163.com>
2017-08-03 18:27:01 +0800

    first ci
    first ci

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by 阳光E购 on 2017/8/3.
+//  Copyright © 2017年 阳光E购. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var totalScore: Int = 0
+    var round: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetValueLabel: UILabel!
+    @IBOutlet weak var totalScoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startNewRound()
+        updateLabels()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        totalScore += points
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+    }
+
+    func updateLabels() {
+        targetValueLabel.text = String(targetValue)
+        totalScoreLabel.text = "\(totalScore)"
+        roundLabel.text = String(round)
+    }
+}
+
